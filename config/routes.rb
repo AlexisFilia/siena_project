810255fd3ed850f9f1a8bc21e981386fe436853d
filia alexis <filia.alexis@gmail.com>
2021-03-08 21:39:51 +0100

    La première page avec les liens vers les autres

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,19 @@ Rails.application.routes.draw do
       resources :poll_user_links, only: [:index, :show, :new, :create]
     end
     resources :roles, only: [:index, :show]
-    resources :teams, only: [:index, :show], shallow: true do
-      resources :team_quest_links, only: [:index]
-      resources :votes, only: [:index, :new, :create]
-    end
+
     resources :levels, only: [:index, :show]
     resources :quests, only: [:index, :show]
     resources :tags, only: [:index]
     resources :user_role_links, only: [:index, :show]
   end
+
+  resources :teams, only: [:index, :show], shallow: true do
+    resources :team_quest_links, only: [:index]
+    resources :votes, only: [:index, :new, :create]
+  end
+
+  resources :tinder_quests, only: [:index]
 
   get '/tim_tests', to: 'pages#tim_tests'
 end
