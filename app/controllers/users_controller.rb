16153baa47580264816d2d7e30dbc2d32af89230
filia alexis <filia.alexis@gmail.com>
2021-03-27 20:45:03 +0100

    Un peu de bootstrap sur les views, user#show et user#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    current_user.update!(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description, :photo)
   end
 end
