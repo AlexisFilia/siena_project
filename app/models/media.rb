34ae03ef5eabf3092c16bf46dda438bfcaa8c780
filia alexis <filia.alexis@gmail.com>
2021-03-30 19:13:10 +0200

    connect media à message et mettre en place les medias et les message_ref dans la messagerie

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,8 +1,19 @@
 class Media < ApplicationRecord
-  belongs_to :team_quest_link
+  belongs_to :message, optional: true
+  belongs_to :team_quest_link, optional: true
 
   validates :url, presence: true
   validates :url, uniqueness: {
     message: 'This url already exists in the DB.'
   }
+
+  def self.define_media(url, type_of, entity)
+    media = if Media.exists?(url: url, type_of: type_of)
+              Media.find_by(url: url, type_of: type_of)
+            else
+              Media.new(url: url, type_of: type_of)
+            end
+    media.send("#{entity.class.to_s.downcase}_id=", entity.id)
+    media.save!
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
+
   has_one :team, through: :user
+  has_many :medias
 
   validates :type_of, presence: true
 end
