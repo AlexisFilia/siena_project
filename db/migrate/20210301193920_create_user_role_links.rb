c4b813fcd378945f9206884469c81a114e3eb70e
filia alexis <filia.alexis@gmail.com>
2021-04-04 16:57:05 +0200

    Les roles sont en place et s'affichent sur le profile et la team + un petit truc pour n'avoir l'upload des docs uniquement sur les quests non soumises

--- db/migrate/20210301193920_create_user_role_links.rb
+++ db/migrate/20210301193920_create_user_role_links.rb
@@ -3,7 +3,7 @@ class CreateUserRoleLinks < ActiveRecord::Migration[6.1]
     create_table :user_role_links do |t|
       t.references :user, null: false, foreign_key: true
       t.references :role, null: false, foreign_key: true
-      t.boolean :on_boarding
+      t.boolean :on_boarding, default: false
 
       t.timestamps
     end
