c71385acb4a8204dc373635ca2262600478f8ae5
filia alexis <filia.alexis@gmail.com>
2021-04-21 12:33:46 +0200

    Les 20% pour valider les quest sont calculé sans les utilisateurs de l'equipe qui soumet le TQL

--- app/models/team_quest_link.rb
+++ app/models/team_quest_link.rb
@@ -16,17 +16,11 @@ class TeamQuestLink < ApplicationRecord
   }
 
   def check_and_update_tql_status_from_votes
-
-  # OBJECTIF : Centralisation de la gestion de la validation des tql ici
-  #chron_tql_check_and_update (avec en commentaire là où le chro se declenche - lib/tasks/chron_tql_check_and_update.rake )
-  #vote_tql_check_and_update
-  #3eme avec probablement des points communs entre les deux
-
     all_votes = self.votes
     company = self.company
     number_of_users = company.users.count
-    #### Ne rien faire si on a moins de 20% des users qui ont voté ####
-    return if all_votes.count < number_of_users / 5
+    #### Ne rien faire si on a moins de 20% des users (excluant l'equipe qui a submit le tql) qui ont voté ####
+    return if all_votes.count < (number_of_users - self.team.users.count) / 5
     #### Si non, changer le status du team_quest_link ####
     self.update_tql_status(all_votes)
   end
@@ -46,4 +40,3 @@ class TeamQuestLink < ApplicationRecord
     end
   end
 end
-
