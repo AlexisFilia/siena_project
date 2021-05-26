717800656de1c97dbb2e527127d72eaf3c3855ac
Timothee Boingneres <timboing@icloud.com>
2021-05-26 19:24:15 +0200

    okaaaay on a une home page qui redirect sur le level#show si on est loggé et j' ai aussi configuré la possibilité de mettre des particules sur la home en changeant l' aspect via le asset/json nouvellement créé. A voir si ca marche en prod

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :tim_tests ]
 
   def home
+
+    if user_signed_in?
+      redirect_to levels_path
+    end
+
   end
 
   def tim_tests
