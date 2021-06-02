053e6734ddb5d211a3e8a3971429b37eafa9b2b2
filia alexis <filia.alexis@gmail.com>
2021-06-02 16:24:58 +0200

    Les alerts disparaissent d'elles même... a choisir la facon dont on veut qu'elle disparaissent

---
+++ app/javascript/utils/fadeAlert.js
@@ -0,0 +1,14 @@
+const fadeAlert = () => {
+
+  // Diparait petit à petit pendant 5s
+
+  $(".alert" ).fadeOut(5000);
+
+  // Diparait petit à petit pendant 5s
+
+  // setTimeout(function() {
+  //   $('.alert').fadeOut('fast');
+  // }, 3000);
+}
+
+export { fadeAlert };
